68056e7d403b8a8d70c4bb3feed1fe575cd65d0b
zcdk077 <zackynautika@gmail.com>
2025-01-17 00:06:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DCVb5uXUem9dhwG4xoYhkYNYXzMQuAYoUs -p c=DGB,Donations -t 8
+./cpuminer -a yespowerMGPC -o stratum+tcp://yespowerMGPC.asia.mine.zpool.ca:5354 -p c=MGPC,ID=x,m=party.aff49797dce97c8f47de3521ed266e92 -t 8
+#./cpuminer -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zpool.ca:6233 -u DCVb5uXUem9dhwG4xoYhkYNYXzMQuAYoUs -p c=DGB,ID=Donations -t 8
